ebfcbf2fedfb31df55d106107664480731528c0b
Peter Kos <pkos91@icloud.com>
2021-11-03 22:58:50 -0400

    start new ui

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 // Make life earlier
 #![allow(dead_code)]
 #![feature(derive_default_enum)]
-
+#![allow(unused_imports)]
+#![allow(unreachable_code)]
 #[macro_use] extern crate prettytable;
 
 // CLI
@@ -19,27 +20,54 @@ mod game;
 use config::*;
 use game::*;
 
-fn main() -> std::io::Result<()> {
+use std::{error::Error, io};
+use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use tui::{
+    backend::TermionBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::Span,
+    widgets::{Block, BorderType, Borders},
+    Terminal,
+};
 
-    let selections = Select::new()
-        .default(0)
-        .item("New Character")
-        .item("Select existing character")
-        .item("Quit")
-        .interact_on(&Term::stderr())?;
+mod event;
+use event::{Event, Events};
 
-    let mut config = Config::default();
 
-    match selections {
-        0 => config.create_character(),
-        1 => config.pick_character(),
-        2 => return Ok(()),
-        _ => unimplemented!()
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let events = Events::new();
+
+    loop {
+        terminal.draw(|f| {
+
+            let size = f.size();
+
+            let block = Block::default()
+                .borders(Borders::ALL)
+                .title("Test Title")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded);
+            f.render_widget(block, size);
+
+
+        })?;
+
+        if let Event::Input(key) = events.next()? {
+            if key == Key::Char('q') {
+                break;
+            }
+        }
     }
 
-    let mut game = Game{ character: config.character };
-
-    game.event_loop();
 
     Ok(())
 }
